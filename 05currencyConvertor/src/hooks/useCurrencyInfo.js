228b2d8cc4b6e6f8c0af1ff3fcbd194c84511002
Suraj Kumar <surajkumar06174@gmail.com>
2025-03-06 01:13:36 +0530

    custom hook

--- 05currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 05currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,5 +1,15 @@
 import { useState, useEffect } from "react";
 
-function UseCurrencyInfo (currency){
-    return ();
-}
+function useCurrencyInfo (currency){
+    const [data, setData] = useState({})
+    useEffect(()=> {
+        fetch(`https://api.frankfurter.app/latest?from=${currency}`)
+        .then((res)=> res.json())
+        .then ((res)=> setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data)
+    return data
+} 
+
+export default useCurrencyInfo;
