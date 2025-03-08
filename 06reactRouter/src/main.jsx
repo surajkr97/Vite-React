2bde58be7b55cfb07d6a07cfdb346d62dd3749bc
Suraj Kumar <surajkumar06174@gmail.com>
2025-03-08 22:23:00 +0530

    initial react router live

--- 06reactRouter/src/main.jsx
+++ 06reactRouter/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+
+
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home />}/>
+      <Route path ='about' element={<About />}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
