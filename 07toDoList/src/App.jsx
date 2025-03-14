bd72459f49c189c64de401933e1a6ca101c5b0d1
Suraj Kumar <surajkumar06174@gmail.com>
2025-03-14 15:21:29 +0530

    update tailwind suggestions

---
+++ 07toDoList/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="container mx-auto">
+        <h1 className="bg-blue-500 text-white text-center">
+          Hello World!
+        </h1>
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ 07toDoList/tailwind.config.js
@@ -0,0 +1,5 @@
+// tailwind.config.js
+export default {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Update paths to match your project
+    plugins: [], // Optional: Add plugins if needed
+  };
