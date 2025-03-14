edc8b0b11d21819650e9c5522365ab1337e40e77
Suraj Kumar <surajkumar06174@gmail.com>
2025-03-14 13:45:56 +0530

    initialize toDoList

---
+++ 07toDoList/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-blue-500 text-center' class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
