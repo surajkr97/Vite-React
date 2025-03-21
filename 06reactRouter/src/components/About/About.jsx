e9950dc9ceb1cacc34aa9e0959da68c00b8a53a2
Suraj Kumar <surajkumar06174@gmail.com>
2025-03-21 16:53:34 +0530

    fix/typo

--- 06reactRouter/src/components/About/About.jsx
+++ 06reactRouter/src/components/About/About.jsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import Image3 from '../../assets/Images/Image3.png'
+import React from "react";
+import Image3 from "../../assets/Images/Image3.png";
 
 export default function About() {
-    return (
-        <div className="py-16 bg-white">
-            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                    <div className="md:5/12 lg:w-5/12">
-                        <img
-                            src={Image3}
-                            alt="image"
-                        />
-                    </div>
-                    <div className="md:7/12 lg:w-6/12">
-                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                            React development is carried out by passionate developers
-                        </h2>
-                        <p className="mt-6 text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
-                        </p>
-                        <p className="mt-4 text-gray-600">
-                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="py-16 bg-white">
+      <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+          <div className="md:5/12 lg:w-5/12">
+            <img src={Image3} alt="image" />
+          </div>
+          <div className="md:7/12 lg:w-6/12">
+            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+              React development is carried out by passionate developers
+            </h2>
+            <p className="mt-6 text-gray-600">
+              Building innovative solutions, creating seamless user experiences,
+              and enhancing performance with cutting-edge technologies!
+              Developers thrive on challenges, crafting scalable applications
+              with precision and creativity.
+            </p>
+            <p className="mt-4 text-gray-600">
+              They solve complex problems, ensuring reliability, security, and
+              efficiency at every stage. Continuous learning and adaptation
+              drive progress in the ever-evolving world of web development.
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- 06reactRouter/src/main.jsx
+++ 06reactRouter/src/main.jsx
@@ -6,6 +6,7 @@ import Home from './components/Home/Home'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import User from './components/User/User'
+import Github from './components/Github/Github'
 
 
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
@@ -13,10 +14,11 @@ import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home />}/>
-      <Route path ='about' element={<About />}/>
-      <Route path = 'contact' element={<Contact />}/>
-      <Route path='user/:userid' element={<User />}/>
+      <Route path='' element={<Home/>}/>
+      <Route path ='about' element={<About/>}/>
+      <Route path = 'contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
+      <Route path='github' element={<Github/>}/>
     </Route>
   )
 )
