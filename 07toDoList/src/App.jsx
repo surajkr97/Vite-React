a83b1c3424850675dbe7a41ab495724b887ea22b
Suraj Kumar <surajkumar06174@gmail.com>
2025-03-27 00:49:25 +0530

    feat(ui)/enhancement

--- 07toDoList/src/App.jsx
+++ 07toDoList/src/App.jsx
@@ -1,20 +1,40 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
 import { useEffect, useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import { RiDeleteBin6Line } from "react-icons/ri";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(()=>{
+  const savedTodos = localStorage.getItem("todos");
+  return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  // useEffect(() => {
+  //   const savedTodos = localStorage.getItem("todos");
+  //   if (savedTodos) setTodos(JSON.parse(savedTodos));
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
 
   const handleAdd = () => {
+    if (!todo.trim()) return;
     setTodos([{ id: uuidv4(), todo, isCompleted: false }, ...todos]);
     setTodo("");
-    saveToLS();
+  };
+
+  const handleKey = (e) => {
+    if (e.key === "Enter") {
+      handleAdd();
+    }
   };
 
   const handleCheckBox = (e) => {
@@ -25,7 +45,6 @@ function App() {
     let newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
-    saveToLS();
   };
 
   const handleEdit = (e, id) => {
@@ -35,52 +54,42 @@ function App() {
       return item.id !== id;
     });
     setTodos(newTodos);
-    saveToLS();
   };
 
   const handleDelete = (e, id) => {
     console.log(`this is id :- ${id}`);
     const updatedTodos = todos.filter((item) => item.id !== id);
     setTodos(updatedTodos);
-    saveToLS();
   };
-
-  const saveToLS = () => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  };
-
-  useEffect(() => {
-    let todoString = localStorage.getItem("todos");
-    if (todoString) {
-      let todos = JSON.parse(localStorage.getItem("todos"));
-      setTodos(todos);
-    }
-  }, []);
 
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-200 min-h-[88vh]">
+      <div className="container mx-auto m-5 rounded-xl p-5 bg-violet-200 min-h-[88vh] md:w-1/2 ">
+        <h1 className="text-3xl font-bold flex justify-center m-2">iTask - Manage Your Todos At One Place</h1>
         <div className="addTodo">
-          <h2 className="text-xl font-bold my-5">Add a Todo</h2>
+          <h2 className="text-xl font-bold my-5"> ⦿ Add a Todo</h2>
+          <div className="flex">
           <input
             onChange={handleChange}
+            onKeyDown={handleKey}
             value={todo}
-            className="bg-amber-50 rounded-lg px-2 py-1 w-1/2"
+            className="bg-amber-50 rounded-full px-2 py-1 w-full"
             type="text"
           />
           <button
-            disabled={todo.length <= 1}
+            disabled={todo.length === 0}
             onClick={handleAdd}
-            className="disabled:bg-violet-800 bg-violet-800 hover:bg-violet-950 text-white font-bold px-2 py-1 rounded-md mx-6"
+            className="disabled:bg-violet-500 bg-violet-800 hover:bg-violet-950 text-white font-bold p-2 rounded-full ml-2 w-20 cursor-pointer"
           >
             Save
           </button>
+          </div>
         </div>
-        <h2 className="text-xl font-bold my-4">Your Todos</h2>
+        <h2 className="text-xl font-bold mt-8 mb-2">☞ Your Todos</h2>
         <div className="todos">
           {todos.length === 0 && (
-            <div>Your todo list is empty. Start by adding a new task! ✨</div>
+            <div className="text-center text-gray-700 italic">Your todo list is empty. Start by adding a new task! ✨</div>
           )}
           {todos.map((item) => {
             return (
@@ -95,24 +104,24 @@ function App() {
                 <div
                   className={`${
                     item.isCompleted ? "line-through" : ""
-                  } bg-violet-50 text-xl px-2 rounded-md w-4/9`}
+                  } flex items-center text-xl px-2 rounded-md w-1/2`}
                 >
                   {item.todo}
                 </div>
                 <div className="buttons my-1">
                   <button
                     onClick={(e) => handleEdit(e, item.id)}
-                    className="bg-violet-800 hover:bg-violet-950 text-white font-bold px-2 rounded-md mx-2 py-1"
+                    className="bg-violet-800 hover:bg-violet-950 text-white font-bold px-2 rounded-md mx-2 py-1 cursor-pointer"
                   >
-                    Edit
+                    <FaEdit />
                   </button>
                   <button
                     onClick={(e) => {
                       handleDelete(e, item.id);
                     }}
-                    className="bg-violet-800 hover:bg-violet-950 text-white font-bold px-2 rounded-md mx-2 py-1"
+                    className="bg-violet-800 hover:bg-violet-950 text-white font-bold px-2 rounded-md py-1 cursor-pointer"
                   >
-                    Delete
+                    <RiDeleteBin6Line />
                   </button>
                 </div>
               </div>
--- 07toDoList/src/components/Navbar.jsx
+++ 07toDoList/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { SiTodoist } from "react-icons/si";
 
 function Navbar() {
   return (
     <nav className="flex justify-between bg-violet-800 text-white py-3">
         <div className='logo'>
-            <span className="font-bold text-2xl mx-10">iTask</span>
+            <span className="flex font-bold text-2xl md:mx-10 mx-5"><SiTodoist  className='m-1'/>iTask</span>
         </div>
-        <ul className="flex gap-10 mx-10">
+        <ul className="flex gap-5 mx-5 md:gap-10 md:mx-10">
             <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
             <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
         </ul>
