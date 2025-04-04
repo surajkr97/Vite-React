b51e8cae39330cf80a9b38a6b8081d0dd34287fb
Suraj Kumar <surajkumar06174@gmail.com>
2025-04-04 20:41:09 +0530

    feat/added-InputBox

--- 05currencyConvertor/src/App.jsx
+++ 05currencyConvertor/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
+import InputBox from './components/InputBox'
 
 const App = () => {
   return (
     <div>
+      <InputBox/>
       <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
--- 05currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 05currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 
-function useCurrencyInfo (currency){
-    const [data, setData] = useState({})
-    useEffect(()=> {
-        fetch(`https://api.frankfurter.app/latest?from=${currency}`)
-        .then((res)=> res.json())
-        .then ((res)=> setData(res[currency]))
-        console.log(data);
-    }, [currency])
-    console.log(data)
-    return data
-} 
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(`https://api.frankfurter.app/latest?from=${currency}`)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
+}
 
 export default useCurrencyInfo;
