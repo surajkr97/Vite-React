4d8da133f3bdf32141a322d165b959af6a38e8a5
Suraj Kumar <surajkumar06174@gmail.com>
2025-04-11 00:40:15 +0530

    added-08miniContext

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='font-extrabold bg-red-500 flex justify-center'>Hello World!!</p>
+    </>
+  )
+}
+
+export default App
