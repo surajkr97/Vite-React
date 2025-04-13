411a1e0b27a678c6c8ff5c345d27e18d36f76db8
Suraj Kumar <surajkumar06174@gmail.com>
2025-04-13 22:15:52 +0530

    refactor: clean up App component layout and useEffect for theme management

--- 10themeSwitcher/src/App.jsx
+++ 10themeSwitcher/src/App.jsx
@@ -15,18 +15,16 @@ function App() {
     setThemeMode("dark");
   };
 
-  useEffect(()=>{
-    document.querySelector('html').classList.remove("light", "dark");
-    document.querySelector('html').classList.add(themeMode)
-  },[themeMode])
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
     <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-          <ThemeBtn />
-          <div className="w-full max-w-sm mx-auto"></div>
+      <div className="flex flex-col min-h-screen items-center justify-center">
+        <div className="m-2"><ThemeBtn /></div>
+        <div className="w-full max-w-sm mx-auto">
           <Card />
         </div>
       </div>
