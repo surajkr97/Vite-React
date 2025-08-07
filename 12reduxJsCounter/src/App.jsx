194391366cfc8e7d066ab6b5be20a211f1741f40
Suraj Kumar <surajkumar06174@gmail.com>
2025-08-07 17:10:46 +0530

    feat: integrate Redux for state management with counter functionality

--- 12reduxJsCounter/src/App.jsx
+++ 12reduxJsCounter/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/slices/counter";
+import DisplayCount from "./components/DisplayCount";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Redux Counter App</div>
+      <div>Count is {count}</div>
+      <button onClick={()=> dispatch(increment())}>+</button>
+      <button onClick={()=> dispatch(decrement())}>-</button>
+      <DisplayCount />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 12reduxJsCounter/src/components/DisplayCount.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+
+const DisplayCount = () => {
+    const count = useSelector((state) => state.counter.value);
+
+  return (
+    <h1>
+      This is the Second Count Dispaly: {count}
+    </h1>
+  )
+};
+
+export default DisplayCount;
